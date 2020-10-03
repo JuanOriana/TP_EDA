61b5d3af068e82032d84d415160daaa77df3cca6
Juan Oriana <joriana@itba.edu.ar>
2020-10-03 12:42:46 -0300

    Set up nodos de linea

--- src/main/java/model/Graph.java
+++ src/main/java/model/Graph.java
@@ -1,10 +1,11 @@
 package model;
 
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.PriorityQueue;
+import com.opencsv.CSVReader;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
 public class Graph {
     private HashSet<Node> nodes = new HashSet<>(); //A ser remplazado con la data-struct correspondiente.
@@ -19,6 +20,32 @@ public class Graph {
 
     Node findNearestPoint(Pair<Double,Double> coordinates){
         throw new UnsupportedOperationException();
+    }
+
+    void setUp() throws IOException {
+        CSVReader reader = new CSVReader(new FileReader("./resources/espacios-culturales.csv"));
+        String[] nextLine;
+        int routeId=-1;
+        int directionId=-1;
+        int newRoute;
+        int newDirection;
+        ArrayList<Node> lineNodes = new ArrayList<Node>();
+        reader.readNext();
+        while((nextLine = reader.readNext()) != null) {
+            newDirection=Integer.parseInt(nextLine[1]);
+            newRoute= Integer.parseInt(nextLine[2]);
+            if (newRoute!=routeId||newDirection!=directionId){
+                loadLine(lineNodes,routeId,directionId);
+                lineNodes = new ArrayList<Node>();
+                directionId=newDirection;
+                routeId=newRoute;
+            }
+            lineNodes.add(new Node(nextLine[8],new Pair<Double,Double>(Double.parseDouble(nextLine[3]),Double.parseDouble(nextLine[4]))));
+        }
+    }
+
+    void loadLine(List<Node> lineNodes, int routeId, int directionId ){
+
     }
 
     void printDijkstra(Node start, Node end){
